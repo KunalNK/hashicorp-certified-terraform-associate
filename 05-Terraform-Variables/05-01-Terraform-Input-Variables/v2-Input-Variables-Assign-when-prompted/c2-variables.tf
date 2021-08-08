7d595e6fcb00c994a5f95ac9ae7c489f9fd655db
Kunal <knkillekar@gmail.com>
2021-08-08 19:23:52 +0530

    updated

--- 05-Terraform-Variables/05-01-Terraform-Input-Variables/v2-Input-Variables-Assign-when-prompted/c2-variables.tf
+++ 05-Terraform-Variables/05-01-Terraform-Input-Variables/v2-Input-Variables-Assign-when-prompted/c2-variables.tf
@@ -2,7 +2,7 @@
 variable "aws_region" {
   description = "Region in which AWS resources to be created"
   type        = string
-  default     = "us-east-1"
+  default     = "ap-south-1"
 }
 
 variable "ec2_ami_id" {
