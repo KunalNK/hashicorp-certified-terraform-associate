b6b0118c6d4aa3046ec623bd53bf5612c2c43e6e
Kunal <knkillekar@gmail.com>
2021-08-08 20:28:57 +0530

    updated

--- 05-Terraform-Variables/05-01-Terraform-Input-Variables/v6-Input-Variables-Assign-with-tfvars-var-file/c2-variables.tf
+++ 05-Terraform-Variables/05-01-Terraform-Input-Variables/v6-Input-Variables-Assign-with-tfvars-var-file/c2-variables.tf
@@ -2,19 +2,19 @@
 variable "aws_region" {
   description = "Region in which AWS resources to be created"
   type        = string
-  default     = "us-east-1"
+  default     = "ap-south-1"
 }
 
 variable "ec2_ami_id" {
   description = "AMI ID"
   type        = string
-  default     = "ami-0915bcb5fa77e4892" # Amazon2 Linux AMI ID
+  default     = "ami-04db49c0fb2215364" # Amazon2 Linux AMI ID
 }
 
 variable "ec2_instance_count" {
   description = "EC2 Instance Count"
   type        = number
-  default     = 2
+  default     = 1
 }
 
 variable "ec2_instance_type" {
