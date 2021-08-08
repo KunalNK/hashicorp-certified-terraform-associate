b616734e4b2de637213e3f302cfcca8d711d31b8
Kunal <knkillekar@gmail.com>
2021-08-08 14:16:28 +0530

    update

--- 04-Terraform-Resources/04-01-Resource-Syntax-and-Behavior/terraform-manifests/c2-ec2-instance.tf
+++ 04-Terraform-Resources/04-01-Resource-Syntax-and-Behavior/terraform-manifests/c2-ec2-instance.tf
@@ -2,9 +2,9 @@
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
 
 resource "aws_instance" "my-ec2-vm" {
-  ami               = "ami-047a51fa27710816e"
+  ami               = "ami-04db49c0fb2215364"
   instance_type     = "t2.micro"
-  availability_zone = "us-east-1a"
+  availability_zone = "ap-south-1a"
   #availability_zone = "us-east-1b"
   tags = {
     "Name" = "web"
