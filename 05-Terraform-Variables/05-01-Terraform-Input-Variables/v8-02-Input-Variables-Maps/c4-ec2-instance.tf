fa96f5b47b9bdc3d9b236e42be8bfa82b421833d
Kunal <knkillekar@gmail.com>
2021-08-08 21:17:28 +0530

    updated

--- 05-Terraform-Variables/05-01-Terraform-Input-Variables/v8-02-Input-Variables-Maps/c4-ec2-instance.tf
+++ 05-Terraform-Variables/05-01-Terraform-Input-Variables/v8-02-Input-Variables-Maps/c4-ec2-instance.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "my-ec2-vm" {
   ami                    = var.ec2_ami_id
   #instance_type          = var.ec2_instance_type[0]
-  instance_type = var.ec2_instance_type_map["big-apps"]
+  instance_type = var.ec2_instance_type_map["small-apps"]
   key_name               = "demo-ec2"
   count                  = var.ec2_instance_count
   user_data              = <<-EOF
